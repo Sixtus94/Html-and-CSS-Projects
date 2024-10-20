2a8aae5633aad746c175677da6222dff935d0ce6
Sixtus94 <sixtech39@hotmail.com>
2024-10-20 14:10:17 +0100

    projects1_Expressions alert
     Javascript projects1 alert\main.js

---
+++ Basic_HTML_and_Javascript_1/Basic Javascript Projects/Project1_expressions_alert/JS/Main.js
@@ -0,0 +1,37 @@
+
+//Main.js
+
+//Window.alert method
+window.alert("Hello, World!");
+
+//document.write method
+document.write("\"Greeting, hello, world.\"");
+
+//creating two variables
+let answer1 = "It's alright,";
+let answer2 = "just leave it there";
+
+//displaying the variables using the document.write method
+document.write(answer1 + answer2);
+
+
+
+//Concatenate a Strings
+var B = "\"Good to meet you and it will be nice meeting as friends,"
+    + " because friends indeed don\'t mind thier status and always don\'t mind peoples opinion about them. \" "
+    + "-Mr pally"
+    document.write(B)
+
+    //Multiple Variables in Js
+    var family = "Abraham", Dad = "Jacob", Mom = "Rebbeca", Daughter = "Diana", Son = "Juda";
+
+    //displaying one of the multiple variables
+    document.write( family );
+
+   //an expression (operator)
+    3 * 6
+    document.write(3 * 6);
+    
+
+
+
