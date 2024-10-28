dc0a0de4236abb858a91f30ab6cc548036e6d209
Sixtus94 <sixtech39@hotmail.com>
2024-10-28 12:50:10 +0000

    project2_function.JS
    add new JavaScript projects code to my Github remote repository

---
+++ Basic_HTML_and_Javascript_1/Basic Javascript Projects/Project2_functions/JS/Main.js
@@ -0,0 +1,25 @@
+
+
+//main.js
+
+
+//Two variables
+
+//Code here can not use carName (undefine outside my functions)
+
+
+//Calling a function() with defined variable carName
+function myFunction() {
+  let carName = "Benz";
+
+  //code here use carName
+}
+
+
+//This function use the += operator
+function myFunction() {
+  var sentence = "I am going to the gym" + " "; //variable is created
+  sentence += "for fitness exercise "; //using the += operator to add to the variable
+ //using document.getElementById() 
+
+}
