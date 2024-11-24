bdd86396babe7b7d125603cf788ba58bff48d9a9
Sixtus94 <sixtech39@hotmail.com>
2024-11-24 18:09:29 +0000

    Javascript_Projects_Assignment
    Add new javascript projects Assignment to my GitHub Remote Repository

--- Basic_HTML_and_Javascript_1/Basic Javascript Projects/Project4_dictionaries/JS/main.js
+++ Basic_HTML_and_Javascript_1/Basic Javascript Projects/Project4_dictionaries/JS/main.js
@@ -1,43 +1,28 @@
-
-
 //main.js
 
-// javascript Dictionary Assignment
-
-
-function my_data() {  // Define functions
-var person = {   // variable Key value pair
-    FirstNme : "Mike",
-    LastName : "Porter"
-    age: 23,
-    eyecolor: " blue"
-  };
-
-document.getElementById("Descrip"). innerHTML = person.firstNme + " is " + person.age + " years old.";
-
+// javascript Project4 Dictionary Assignment
+function my_data() {  // Define functions and call
+var person = {   // variable Key value pair, Dictionary.
+firstName:"Mike",
+lastName:"Porter",
+age:23,
+eyecolor:"blue"
+};
+    
+document.getElementById("KVP").innerHTML=person.firstName + " is " + person.age +"years old.";
 }
- 
 
 function my_Dictionary() { //Defining function
-    var Animal = {   //variables key value pair
-        species: "Tiger",
-        color: "speckle",
-        prey: "deer",
-        age:5,
-        sound: "Bark!"
+var Animal = {   //variables key value pair
+species:"Tiger",
+color:"speckle",
+prey:"deer",
+age:5,
+sound:"Bark!"
+};
 
-    };
-    delete Animal.sound;
-    document.getElementById("Dictionary").innerHTML = Animal.sound;
+document.getElementById("Dictionary").innerHTML=" The"+ Animal.species+ " is " + Animal.age + " years old and says " + Animal.sound;
 }
-
-
-
-
-
-
     
-
-
-
-
+    
+    
---
+++ Basic_HTML_and_Javascript_1/Basic Javascript Projects/Project6_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,16 @@
+
+//main.js
+
+
+//The typeof Operator returns the type of a variable of an expression 
+document.write(typeof "Magnus");//print string
+document.write(typeof "Magnus" + "Kelly") //print string
+document.write(typeof 2.23);//print number
+document.write(typeof 25);//print number
+document.write(typeof 31 + 22);//print number
+document.write(typeof "true") //print boolean
+document.write(typeof "false")//print boolean
+document.write(typeof 123n)//print bigint
+document.write(typeof "Symbol()")//print symbol
+document.write (typeof x);//print undefined
+document.write("20" + 5); //print type of coercion or force two number together (project5_comparison_type_Coercion)
